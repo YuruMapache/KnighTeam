2227af232a0e5a616389c39d00a67ac6a073cb72
eduvela <604-eduvela@users.noreply.gitlab.inf.uva.es>
2022-11-12 13:27:55 +0100

    Modelo

---
+++ app/src/main/java/com/example/justajuan/model/BaseItem.java
@@ -0,0 +1,20 @@
+package com.example.justajuan.model;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Timer;
+
+import objeto.Material;
+
+public class BaseItem {
+    private String name;
+    private Duration timeCraft;
+    private ArrayList<Material> materials;
+    private int attack;
+    private int defense;
+
+    public BaseItem(){
+
+    }
+
+}
---
+++ app/src/main/java/com/example/justajuan/model/Material.java
@@ -0,0 +1,5 @@
+package com.example.justajuan.model;
+
+public class Material {
+
+}
