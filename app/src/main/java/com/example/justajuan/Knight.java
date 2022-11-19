0df5542f87c8fa0e35d8ce5d80dfbec34811a796
eduvela <604-eduvela@users.noreply.gitlab.inf.uva.es>
2022-11-19 22:32:17 +0100

    Temporizador. (No probado)

--- app/src/main/java/com/example/justajuan/Knight.java
+++ app/src/main/java/com/example/justajuan/Knight.java
@@ -1,14 +1,23 @@
 package com.example.justajuan;
 
+import android.os.Bundle;
+import android.os.CountDownTimer;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.justajuan.model.Time;
 
 public class Knight extends AppCompatActivity {
+
+    private CountDownTimer globalTimer;
+    private Time glblTimer;      // Textview del tiempo restante del temporizador
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_knight);
+        glblTimer = new Time(findViewById(R.id.timerTextView));
+        glblTimer.startTimer();
     }
+
 }
