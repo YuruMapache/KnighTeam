8d3ce08896d9362136923f96ebaddd03e3eaa516
juatorr <juan.torres@alumnos.uva.es>
2022-12-05 09:22:22 +0100

    actualizando tutorial

--- app/src/main/java/com/example/justajuan/ui/Tutorial.java
+++ app/src/main/java/com/example/justajuan/ui/Tutorial.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
-import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
@@ -47,70 +46,139 @@ public class Tutorial extends AppCompatActivity {
         layoutTiempo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final Dialog acciones = new Dialog(Tutorial.this);
-                acciones.setContentView(R.layout.pop_up_tutorial2);
-                acciones.setCancelable(true);
-                acciones.show();
+                AlertDialog.Builder builder = new AlertDialog.Builder(Tutorial.this, R.style.PopUpTutorialTheme);
+
+                builder.setCancelable(true);
+                builder.setTitle("TIEMPO DISPONIBLE");
+                builder.setMessage("En esta parte de la pantalla tendrás siempre disponible el tiempo restante para " +
+                        "realizar tus diversas acciones.\n" + "Esto se ve reflejado como tiempo hasta vuestra próxima justa.");
+
+                builder.setPositiveButton("¡Entendido!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                builder.show();
             }
         });
 
         layoutRecursos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final Dialog acciones = new Dialog(Tutorial.this);
-                acciones.setContentView(R.layout.pop_up_tutorial1);
-                acciones.setCancelable(true);
-                acciones.show();
+                AlertDialog.Builder builder = new AlertDialog.Builder(Tutorial.this, R.style.PopUpTutorialTheme);
+
+                builder.setCancelable(true);
+                builder.setTitle("RECURSOS");
+                builder.setMessage("Aquí podras ver los materiales disponibles,así como la cantidad que dispones de cada uno de ellos.\n"
+                        + "El personaje te servirá como recordatorio de tu rol y además al final de cada toma de decisiones " +
+                        "te dará los resultados.");
+
+                builder.setPositiveButton("¡Entendido!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                builder.show();
             }
         });
 
         layoutItems.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final Dialog acciones = new Dialog(Tutorial.this);
-                acciones.setContentView(R.layout.pop_up_tutorial3);
-                acciones.setCancelable(true);
-                acciones.show();
+                AlertDialog.Builder builder = new AlertDialog.Builder(Tutorial.this, R.style.PopUpTutorialTheme);
+
+                builder.setCancelable(true);
+                builder.setTitle("ITEMS");
+                builder.setMessage("Desde aquí podrás ver las acciones que ya has comenzado y que están " +
+                        "actualmente en progreso, además del tiempo hasta su finalización.");
+
+                builder.setPositiveButton("¡Entendido!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                builder.show();
             }
         });
 
         botonDesplAcciones.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final Dialog acciones = new Dialog(Tutorial.this);
-                acciones.setContentView(R.layout.pop_up_tutorial4);
-                acciones.setCancelable(true);
-                acciones.show();
+                AlertDialog.Builder builder = new AlertDialog.Builder(Tutorial.this, R.style.PopUpTutorialTheme);
+
+                builder.setCancelable(true);
+                builder.setTitle("ACCIONES");
+                builder.setMessage("El botón Acciones te permitirá ver una lista de actividades disponibles para tu rol");
+
+                builder.setPositiveButton("¡Entendido!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                builder.show();
             }
         });
 
         botonDesplTienda.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final Dialog acciones = new Dialog(Tutorial.this);
-                acciones.setContentView(R.layout.pop_up_tutorial4);
-                acciones.setCancelable(true);
-                acciones.show();
+                AlertDialog.Builder builder = new AlertDialog.Builder(Tutorial.this, R.style.PopUpTutorialTheme);
+
+                builder.setCancelable(true);
+                builder.setTitle("TIENDA");
+                builder.setMessage("Con Comprar podrás acceder a un apartado para intercambiar " +
+                        "diversos objetos disponibles para tu rol a cambio de un módico precio, más fácil que craftearlos a mano.");
+
+                builder.setPositiveButton("¡Entendido!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                builder.show();
             }
         });
 
         botonDesplDiario.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final Dialog acciones = new Dialog(Tutorial.this);
-                acciones.setContentView(R.layout.pop_up_tutorial4);
-                acciones.setCancelable(true);
-                acciones.show();
+                AlertDialog.Builder builder = new AlertDialog.Builder(Tutorial.this, R.style.PopUpTutorialTheme);
+
+                builder.setCancelable(true);
+                builder.setTitle("DIARIO");
+                builder.setMessage("Desde Diario verás un resumen de los mensajes recibidos hasta el momento " +
+                        "de los resultados de cada justa");
+
+                builder.setPositiveButton("¡Entendido!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                builder.show();
             }
         });
 
         botonDesplInventario.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final Dialog acciones = new Dialog(Tutorial.this);
-                acciones.setContentView(R.layout.pop_up_tutorial4);
-                acciones.setCancelable(true);
-                acciones.show();
+                AlertDialog.Builder builder = new AlertDialog.Builder(Tutorial.this, R.style.PopUpTutorialTheme);
+
+                builder.setCancelable(true);
+                builder.setTitle("INVENTARIO");
+                builder.setMessage("En Inventario podrás ver todo lo que tienes almacenado y disponible.");
+
+                builder.setPositiveButton("¡Entendido!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                builder.show();
             }
         });
 
