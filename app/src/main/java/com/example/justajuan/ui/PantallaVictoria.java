48e2e80ab8412d827f19947352a2b3255c1ff386
juatorr <juan.torres@alumnos.uva.es>
2022-12-09 11:43:05 +0100

    annadidas pantalla victoria y pantalla derrota

---
+++ app/src/main/java/com/example/justajuan/ui/PantallaVictoria.java
@@ -0,0 +1,23 @@
+package com.example.justajuan.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.justajuan.R;
+
+public class PantallaVictoria extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_victoria);
+
+    }
+}
