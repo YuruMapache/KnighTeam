b16bb4c9744712151495630cd66bb6bab870536f
juatorr <juan.torres@alumnos.uva.es>
2022-12-11 01:51:31 +0100

    annadida opcion salir app en varias clases

--- app/src/main/java/com/example/justajuan/ui/PantallaResultadosCuestionario.java
+++ app/src/main/java/com/example/justajuan/ui/PantallaResultadosCuestionario.java
@@ -1,4 +1,38 @@
 package com.example.justajuan.ui;
 
-public class PantallaResultadosCuestionario {
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.justajuan.R;
+
+public class PantallaResultadosCuestionario extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_resultados_cuestionario);
+    }
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this, R.style.AlertDialogTheme)
+                .setMessage("¿Quieres cerrar la app?")
+
+                .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        finishAffinity();
+                        System.exit(0);
+                    }
+                })
+
+                .setNegativeButton("No", null)
+                .show();
+    }
 }
--- app/src/main/java/com/example/justajuan/ui/ResultadosCaballero.java
+++ app/src/main/java/com/example/justajuan/ui/ResultadosCaballero.java
@@ -1,7 +1,9 @@
 package com.example.justajuan.ui;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
@@ -18,5 +20,20 @@ public class ResultadosCaballero extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_resultados_caballero);
+    }
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this, R.style.AlertDialogTheme)
+                .setMessage("¿Quieres cerrar la app?")
+
+                .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        finishAffinity();
+                        System.exit(0);
+                    }
+                })
+
+                .setNegativeButton("No", null)
+                .show();
     }
 }
