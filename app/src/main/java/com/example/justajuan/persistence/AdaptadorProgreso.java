34d35e1f2ee95c1ac6a7303fd7ae673496fc381e
Ruben <rubenovi98@gmail.com>
2022-12-11 20:24:06 +0100

    Inicio Parte de pantalla de creación de objetos

--- app/src/main/java/com/example/justajuan/persistence/AdaptadorProgreso.java
+++ app/src/main/java/com/example/justajuan/persistence/AdaptadorProgreso.java
@@ -7,24 +7,25 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.justajuan.R;
 import com.example.justajuan.model.Material;
+import com.example.justajuan.model.Objeto;
 
 import java.util.ArrayList;
 
 
 public class AdaptadorProgreso extends ArrayAdapter {
-    private ArrayList<Material> listaMateriales = new ArrayList<>();
+    private ArrayList<Objeto> listaObjetos =new ArrayList<>();
 
-    public AdaptadorProgreso(Context context, int textViewResourceId, ArrayList objects) {
-        super(context, textViewResourceId, objects);
-        listaMateriales = objects;
+    public AdaptadorProgreso(Context context, int textViewResourceId, ArrayList objects){
+        super(context,textViewResourceId,objects);
+        listaObjetos=objects;
     }
-
     @Override
-    public int getCount() {
+    public int getCount(){
         return super.getCount();
     }
 
@@ -33,20 +34,27 @@ public class AdaptadorProgreso extends ArrayAdapter {
 
         View v = convertView;
         LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        v = inflater.inflate(R.layout.gridview_recursos_feudo, null);
-        TextView textoNombre = (TextView) v.findViewById(R.id.nombreMaterial);
-        ImageView imageView = (ImageView) v.findViewById(R.id.imagenMaterial);
-        TextView textoCantidad = (TextView) v.findViewById(R.id.cantidadMateriales);
-        imageView.setImageResource(listaMateriales.get(position).getIdDrawable());
-        textoNombre.setText(listaMateriales.get(position).getName());
-        textoCantidad.setText(String.valueOf(listaMateriales.get(position).getCantidad()));
+        v = inflater.inflate(R.layout.activity_gridview_materiales, null);
+        TextView textoNombre = (TextView) v.findViewById(R.id.nombreObjetoCreandose);
+        ImageView imageView = (ImageView) v.findViewById(R.id.imagenObjetoCreandose);
+        TextView porcentajeTexto = (TextView) v.findViewById(R.id.porcentajeProgreso);
+        ProgressBar barraProgreso=(ProgressBar) v.findViewById(R.id.barraProgresoObjeto) ;
+        imageView.setImageResource(listaObjetos.get(position).getIdDrawable());
+        textoNombre.setText(listaObjetos.get(position).getNombre());
+        //porcentajeTexto.setText(listaObjetos.get(position).getContador());
+
 
 
         return v;
 
     }
-
-    public void setListaMateriales(ArrayList objects) {
-        listaMateriales = objects;
+    public void setListaMateriales(ArrayList objects){
+        listaObjetos=objects;
     }
+
+
+
+
+
+
 }
