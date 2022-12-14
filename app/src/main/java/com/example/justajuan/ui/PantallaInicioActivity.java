65b1d3a128e92cfc755dee50055057233f2aabde
Ruben <rubenovi98@gmail.com>
2022-12-15 00:39:51 +0100

    a

--- app/src/main/java/com/example/justajuan/ui/PantallaInicioActivity.java
+++ app/src/main/java/com/example/justajuan/ui/PantallaInicioActivity.java
@@ -49,6 +49,7 @@ public class PantallaInicioActivity extends AppCompatActivity {
         botonTutorial.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                SubidaObjetosBD.setObjetos();
                 //Pasa a las ventanas del tutorial
                 startActivity(new Intent(PantallaInicioActivity.this, Tutorial.class));
             }
